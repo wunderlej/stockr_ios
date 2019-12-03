6f56f84df7ce8042f37be6a8582b337641974b41
John Wunderle <john.wunderle@us.sogeti.com>
2019-12-03 09:05:00 -0500

    Initial Commit

---
+++ Stockr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stockr
+//
+//  Created by jwunderl on 12/3/19.
+//  Copyright © 2019 John Wunderle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
